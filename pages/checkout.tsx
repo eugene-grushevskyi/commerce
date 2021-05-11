59ea3ff7cfcb5e5ede84120e600261a3c6d795b8
ievgenii <jimmydeanmaster@gmail.com>
2021-05-11 17:43:29 +0300

    phone mask added

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -2,9 +2,10 @@ import useCart from '@framework/cart/use-cart'
 import usePrice from '@framework/product/use-price'
 import { Layout } from '@components/common'
 import { Button, Input, Text } from '@components/ui'
+import InputMask from 'react-input-mask'
 import { Bag, Cross, Check, MapPin, CreditCard } from '@components/icons'
 import { CartItem } from '@components/cart'
-import { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import useCheckout from '@framework/cart/use-checkout'
 import { Product as CommerceProduct } from '@commerce/types'
 
@@ -125,7 +126,7 @@ export default function Checkout() {
 
   return (
     <div className="grid lg:grid-cols-12 w-full max-w-7xl mx-auto">
-      <div className="lg:col-span-8">
+      <div className={isEmpty ? 'lg:col-span-12' : 'lg:col-span-8'}>
         {isLoading || isEmpty ? (
           <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
             <span className="border border-dashed border-secondary flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
@@ -200,120 +201,122 @@ export default function Checkout() {
           </div>
         )}
       </div>
-      <div className="lg:col-span-4">
-        <div className="flex-shrink-0 px-4 sm:px-6 mt-2">
-          <Text className="mt-4 pb-1">Ім'я</Text>
-          <Input
-            defaultValue={formData?.name}
-            onBlur={(e) => handleChange(e, 'name')}
-          ></Input>
-          <Text className="mt-4 pb-1">Телефон</Text>
-          <Input
-            defaultValue={formData?.phone}
-            onBlur={(e) => handleChange(e, 'phone')}
-          ></Input>
-          <Text className="mt-4 pb-1">Додаткові побажання?</Text>
-          <Input
-            defaultValue={formData?.description}
-            onBlur={(e) => handleChange(e, 'description')}
-          ></Input>
+      {!isEmpty && (
+        <div className="lg:col-span-4">
+          <div className="flex-shrink-0 px-4 sm:px-6 mt-2">
+            <Text className="mt-4 pb-1">Ім'я</Text>
+            <Input
+              defaultValue={formData?.name}
+              onBlur={(e) => handleChange(e, 'name')}
+            ></Input>
+            <Text className="mt-4 pb-1">Телефон</Text>
+            <InputMask
+              value={formData?.phone}
+              onChange={(e) => handleChange(e, 'phone')}
+              mask="+380 (99) 999 99 99"
+            >
+              {(inputProps: unknown) => <Input {...inputProps} type="tel" />}
+            </InputMask>
+            <Text className="mt-4 pb-1">Додаткові побажання?</Text>
+            <Input
+              defaultValue={formData?.description}
+              onBlur={(e) => handleChange(e, 'description')}
+            ></Input>
+          </div>
+          <div className="flex-shrink-0 px-4 py-8 sm:px-6">
+            <div
+              className={
+                (deliver ? 'active-delivery' : '') +
+                ' rounded-md border border-accents-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accents-4'
+              }
+              onClick={() => {
+                setDeliver(true)
+              }}
+            >
+              <div className="mr-5">
+                <MapPin />
+              </div>
+              <div className="text-sm text-center font-medium">
+                <span className="uppercase">Вказати адресу</span>
+              </div>
+            </div>
+            {deliver && (
+              <div className="mb-4">
+                <Text className="mt-4 pb-1">Куди доставити?</Text>
+                <Input
+                  defaultValue={formData?.address}
+                  onBlur={(e) => handleChange(e, 'address')}
+                ></Input>
+              </div>
+            )}
+            <div
+              className={
+                (!deliver ? 'active-delivery' : '') +
+                ' rounded-md border border-accents-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accents-4'
+              }
+              onClick={() => {
+                setDeliver(false)
+              }}
+            >
+              <div className="mr-5">
+                <Bag />
+              </div>
+              <div className="text-sm text-center font-medium">
+                <span className="uppercase">Драйвую пішки</span>
+              </div>
+            </div>
+            <div className="border-t border-accents-2 mb-4 mt-4"></div>
+            <div className="rounded-md border border-accents-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accents-4">
+              <div className="mr-5">
+                <CreditCard />
+              </div>
+              <div className="text-sm text-center font-medium">
+                <span className="uppercase">Оплатити картою</span>
+              </div>
+            </div>
+            <div className="border-t border-accents-2">
+              <ul className="py-3">
+                <li className="flex justify-between py-1">
+                  <span>Сума</span>
+                  <span>{subTotal}</span>
+                </li>
+                <li className="flex justify-between py-1">
+                  <span>Пакування у закладі</span>
+                  <span>{packagingPrice}</span>
+                </li>
+                <li className="flex justify-between py-1">
+                  <span>Вартість доставки</span>
+                  <span className="font-bold tracking-wide">
+                    {deliver ? deliverPrice : 'FREE'}
+                  </span>
+                </li>
+              </ul>
+              <div className="flex justify-between border-t border-accents-2 py-3 font-bold mb-10">
+                <span>Всього</span>
+                <span>{total}</span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-end">
+              <div className="w-full lg:w-72">
+                {isEmpty ? (
+                  <Button href="/" Component="a" width="100%">
+                    Хочу ще чогось
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={checkout}
+                    disabled={!!order}
+                    Component="a"
+                    width="100%"
+                  >
+                    Замовити
+                  </Button>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex-shrink-0 px-4 py-8 sm:px-6">
-          <div
-            className={
-              (deliver ? 'active-delivery' : '') +
-              ' rounded-md border border-accents-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accents-4'
-            }
-            onClick={() => {
-              setDeliver(true)
-            }}
-          >
-            <div className="mr-5">
-              <MapPin />
-            </div>
-            <div className="text-sm text-center font-medium">
-              <span className="uppercase">Вказати адресу</span>
-            </div>
-          </div>
-          {deliver && (
-            <div className="mb-4">
-              <Text className="mt-4 pb-1">Куди доставити?</Text>
-              <Input
-                defaultValue={formData?.address}
-                onBlur={(e) => handleChange(e, 'address')}
-              ></Input>
-            </div>
-          )}
-          <div
-            className={
-              (!deliver ? 'active-delivery' : '') +
-              ' rounded-md border border-accents-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accents-4'
-            }
-            onClick={() => {
-              setDeliver(false)
-            }}
-          >
-            <div className="mr-5">
-              <Bag />
-            </div>
-            <div className="text-sm text-center font-medium">
-              <span className="uppercase">Драйвую пішки</span>
-            </div>
-          </div>
-          <div className="border-t border-accents-2 mb-4 mt-4"></div>
-          {/* Payment Method */}
-          {/* Only available with customCheckout set to true - Meaning that the provider does offer checkout functionality. */}
-          <div className="rounded-md border border-accents-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accents-4">
-            <div className="mr-5">
-              <CreditCard />
-            </div>
-            <div className="text-sm text-center font-medium">
-              <span className="uppercase">Оплатити картою</span>
-              {/* <span>VISA #### #### #### 2345</span> */}
-            </div>
-          </div>
-          <div className="border-t border-accents-2">
-            <ul className="py-3">
-              <li className="flex justify-between py-1">
-                <span>Сума</span>
-                <span>{subTotal}</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Пакування у закладі</span>
-                <span>{packagingPrice}</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Вартість доставки</span>
-                <span className="font-bold tracking-wide">
-                  {deliver ? deliverPrice : 'FREE'}
-                </span>
-              </li>
-            </ul>
-            <div className="flex justify-between border-t border-accents-2 py-3 font-bold mb-10">
-              <span>Всього</span>
-              <span>{total}</span>
-            </div>
-          </div>
-          <div className="flex flex-row justify-end">
-            <div className="w-full lg:w-72">
-              {isEmpty ? (
-                <Button href="/" Component="a" width="100%">
-                  Хочу ще чогось
-                </Button>
-              ) : (
-                <Button
-                  onClick={checkout}
-                  disabled={!!order}
-                  Component="a"
-                  width="100%"
-                >
-                  Замовити
-                </Button>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
